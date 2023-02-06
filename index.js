3faa7e2334c70be3a72b20bf17dc4be111234bc0
Dominika <124594522+11Dominika@users.noreply.github.com>
2023-02-06 17:47:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+// let bac="apple phone"
+// let hat="ORANGE"
+// console.log(bac.replace("phone","banana"))
+// console.log(bac.toUpperCase())
+// console.log(hat.toLowerCase())
+// function a(b,c) {
+//     let d=0
+//    d=b+c
+//    console.log(d)
+// }
+// a(3,4)
+// a(10,6)
+// a(3,8)
+
+
+function code(r,s) {
+    let g=0
+   g=r*s
+   console.log(g)
+}
+code(2,2)
+code(30,1)
+code(15,7)
+function first(str){
+    let newstr=str[0].toUpperCase()+ str.slice(1)
+    console.log(newstr)
+
+
+}
+first("pen")
